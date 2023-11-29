5d779e2d57f48c8c1e631ab5247d2a3da93e7dbe
whosefriendA <2653344989@qq.com>
2023-11-29 16:58:02 +0800

    new

--- cpp/cpp13.c
+++ cpp/cpp13.c
@@ -23,14 +23,28 @@
 //     return 0;
 // }
 //2.
-int main(int argc,char*argv[]){
-    if(argc<3)
-    exit(EXIT_FAILURE);
-     FILE *fp1;
-     FILE *fp2;
-    if((fp1=fopen(argv[1],"rb"))==NULL)
-    exit(EXIT_FAILURE);
-    if((fp2=fopen(argv[2],"wb"))==NULL)
-    exit(EXIT_FAILURE);
-    
-}
+// int main(int argc,char*argv[]){
+//     if(argc<3)
+//     exit(EXIT_FAILURE);
+//     char ch[1000];
+//      FILE *fp1;
+//      FILE *fp2;
+//     if((fp1=fopen(argv[1],"rb"))==NULL)
+//     exit(EXIT_FAILURE);
+//     if((fp2=fopen(argv[2],"wb"))==NULL)
+//     exit(EXIT_FAILURE);
+//     for(int i=0;(ch[i]!=getc(fp1))!=EOF;i++)
+//     fclose(fp1);
+//     fprintf(fp2,"%s",ch);
+//     fclose(fp2);
+// }
+//4.
+// int main(int argc,char*argv[])
+// {
+//     if(argc<2)
+//     exit(EXIT_FAILURE);
+//     for(int i=argc;i!=1;i--)
+//     {
+//         printf("%s",argv[i]);
+//     }
+// }
