b44ca404ccea42313932a1451529d7d35e297c41
whosefriendA <2653344989@qq.com>
2023-12-03 19:09:38 +0800

    new

---
+++ cpp/cpp16,17.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//2.
+// #define TAVE(a,b) 1/((1/a+b/a)/2)
+// int main(){
+//     int a;
+//     int b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d",TAVE(a,b));
+// }
+//6.
+// int search(int*,int,int);
+// int main()
+// {
+//     int num[10];
+//     int x;
+//     scanf("%d",&x);
+//     for(int i=0;i<10;i++)
+//     scanf("%d",num[i]);
+//     printf("%d",search(num,10,x));
+// }
+// int search(int*num,int size,int x)
+// {
+//     for(int i=0;i<size;i++)
+//     if(num[i]==x)
+//     return 1;
+// return 0;
+// }
