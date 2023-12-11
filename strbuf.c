8cc3ae7164d8e41742c2a4437394f0cc3e82d3e5
whosefriendA <2653344989@qq.com>
2023-12-11 20:40:29 +0800

    new

--- strbuf.c
+++ strbuf.c
@@ -1,70 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-struct strbuf{
-  int len;     //当前缓冲区（字符串）长度
-  int alloc;   //当前缓冲区（字符串）容量
-  char *buf;   //缓冲区（字符串）
-};
-//PART:A的函数原型
-void strbuf_init(struct strbuf *sb,size_t alloc);
-void strbuf_attach(struct strbuf *sb, void *str, size_t len, size_t alloc);
-void strbuf_release(struct strbuf *sb);
-void strbuf_swap(struct strbuf *a, struct strbuf *b);
-char *strbuf_detach(struct strbuf *sb, size_t *sz);
-int strbuf_cmp(const struct strbuf *first, const struct strbuf *second);
-void strbuf_reset(struct strbuf *sb);
-
-//PARTB:B的函数原型
-void strbuf_grow(struct strbuf *sb, size_t extra);
-void strbuf_add(struct strbuf *sb, const void *data, size_t len);
-void strbuf_addch(struct strbuf *sb, int c);
-void strbuf_addstr(struct strbuf *sb, const char *s);
-void strbuf_addbuf(struct strbuf *sb, const struct strbuf *sb2);
-void strbuf_setlen(struct strbuf *sb, size_t len);
-size_t strbuf_avail(const struct strbuf *sb);
-void strbuf_insert(struct strbuf *sb, size_t pos, const void *data, size_t len);
-
-//PART:C的函数原型
-void strbuf_ltrim(struct strbuf *sb);
-void strbuf_rtrim(struct strbuf *sb);
-void strbuf_remove(struct strbuf *sb, size_t pos, size_t len);
-
-//PART:D的函数原型
-ssize_t strbuf_read(struct strbuf *sb, int fd, size_t hint);
-int strbuf_getline(struct strbuf *sb, FILE *fp);
-
-int main(){
-    struct strbuf sbuf1;
-    struct strbuf sbuf2;
-    strbuf_init(&sbuf1,10);
-
-    strbuf_attach(&sbuf1,"xiyou",5,10);
-
-    strbuf_release(&sbuf1);
-
-    strbuf_swap(&sbuf1,&sbuf2);
-
-    int sz=0;
-    char*detachsb=strbuf_detach(&sbuf1,&sz) ;
-
-    if(!strbuf_cmp(&sbuf1,&sbuf2))
-    printf("These two structs is the same");
-    else 
-    printf("These two structs is not the same");
-    
-    reset(&sbuf1);
-
-    int extra=0;
-    strbuf_grow(&sbuf1,extra);
-
-    void*data={'0'};
-    int len=0;
-    strbuf_add(&strbuf,data,len);
-
-}
+#include "strbuf.h"
 //PART:A
-
 //初始化
 void strbuf_init(struct strbuf *sb,size_t alloc){
     sb->len=0;
@@ -77,8 +12,9 @@ void strbuf_attach(struct strbuf *sb, void *str, size_t len, size_t alloc){
         sb->alloc*=2;
         sb->buf=(char*)realloc(sb->buf,sb->alloc);
     }
-    strcpy(sb->buf,str);
+    memcpy(sb->buf+sb->len,str,len);
     sb->len+=len;
+    sb->buf[sb->len]='\0';
 }
 //释放
 void strbuf_release(struct strbuf *sb){
@@ -146,7 +82,7 @@ void strbuf_add(struct strbuf *sb, const void *data, size_t len){
         sb->buf=(char*)realloc(sb->buf,2*sb->alloc);
         sb->alloc*=2;
     }
-    strcat(sb->buf,data);
+    memcpy(sb->buf+sb->len,data,len);
     sb->len+=len;
 }
 //追加一个字符
@@ -155,7 +91,8 @@ void strbuf_addch(struct strbuf *sb, int c){
     sb->buf=(char*)realloc(sb->buf,2*sb->alloc);
     sb->alloc+=1;
     }
-    strcat(sb->buf,c);
+    sb->buf[sb->len]=(char)c;
+    sb->buf[sb->len+1]='\0';
     sb->len+=1;
 }
 //追加一个字符串
@@ -175,7 +112,7 @@ void strbuf_addbuf(struct strbuf *sb, const struct strbuf *sb2){
         sb->buf=(char*)realloc(sb->buf,2*sb->alloc);
         sb->alloc*=2;
     }
-    strcat(sb->buf,sb->buf);
+    memcpy(sb->buf+sb->len,sb2->buf,sb2->len);
     sb->len+=sb2->len;
 }
 //设置字符串长度
@@ -247,18 +184,18 @@ ssize_t strbuf_read(struct strbuf *sb, int fd, size_t hint){
     if(sb->len==sb->alloc)
     sb->buf=(char*)realloc(sb->buf,newalloc*2);
     }
-
 }
 //将文件句柄为 fp 的一行内容（抛弃换行符）读取到 sb
 int strbuf_getline(struct strbuf *sb, FILE *fp){
+    char ch;
      if (sb->buf == NULL) {
         sb->alloc = 128;
         sb->buf = (char *)malloc(sb->alloc);
         sb->len = 0;
     }
-    while ((char c = fgetc(fp)) != EOF && ch != '\n'){
+    while ((ch = fgetc(fp)) != EOF && ch != '\n'){
     if(sb->len==sb->alloc)
-    sb->buf=(char*)realloc(sb->buf,newalloc*2);
+    sb->buf=(char*)realloc(sb->buf,sb->alloc*2);
     sb->buf[sb->len++] = ch;
     }
     sb->buf[sb->len] = '\0';
