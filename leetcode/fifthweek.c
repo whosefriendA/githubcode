07f51a61d10346024a5444034beeef53178b63e3
whosefriendA <2653344989@qq.com>
2023-12-19 20:40:53 +0800

    new

--- leetcode/fifthweek.c
+++ leetcode/fifthweek.c
@@ -16,3 +16,22 @@
 //     }
 //     return n==1;
 // }
+// void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+//  int *arr = (int*)malloc(sizeof(int)*(m+n));
+//     int index=0,left=0,right=0;
+//     while(index<nums1Size&&left<m&&right<n){
+//         if(nums1[left]<nums2[right]){
+//             arr[index++]=nums1[left++];
+//         }else if(nums1[left]>nums2[right]){
+//             arr[index++]=nums2[right++];
+//         }else{
+//             arr[index++]=nums1[left++];
+//             arr[index++]=nums2[right++];
+//         }
+//     }
+//     while(left<m) arr[index++]=nums1[left++];
+//     while(right<n) arr[index++]=nums2[right++];
+//     for(int i=0;i<index;i++){
+//         nums1[i] = arr[i];
+//     }
+// }
