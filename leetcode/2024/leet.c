e4c9b0dbd6e19e2e249dbd8a14165ab806722cd4
whosefriendA <2653344989@qq.com>
2024-03-06 21:54:08 +0800

    new

--- leetcode/2024/leet.c
+++ leetcode/2024/leet.c
@@ -40,3 +40,21 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
      }
 return head;
 }
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* reverseList(struct ListNode* head){
+    //保存cur的下一个结点
+    struct ListNode* temp;
+    //pre指针指向前一个当前结点的前一个结点
+    struct ListNode* pre = NULL;
+    //用head代替cur，也可以再定义一个cur结点指向head。
+    while(head) {
+        //保存下一个结点的位置
+        temp = head->next;
+        //翻转操作
+        head->next = pre;
+        //更新结点
+        pre = head;
+        head = temp;
+    }
+    return pre;
+}
